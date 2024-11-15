9588396b0ea558a15dea52798f9b3738fbb64495
hank940115 <146960977+hank940115@users.noreply.github.com>
2024-11-15 17:16:02 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/types.h>
 
 #define ERR_EXIT(s) perror(s), exit(errno)
 
